0167eb850049a77d3b2ecc8e23407e6409c0447b
Henry McCallaghan <henrymcc@gmail.com>
2016-08-13 14:57:48 +0200

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by david johnson on 2016/08/13.
+//  Copyright © 2016 hiddenplatform. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ testGitTests/testGitTests.swift
@@ -0,0 +1,36 @@
+//
+//  testGitTests.swift
+//  testGitTests
+//
+//  Created by david johnson on 2016/08/13.
+//  Copyright © 2016 hiddenplatform. All rights reserved.
+//
+
+import XCTest
+@testable import testGit
+
+class testGitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
